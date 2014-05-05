293d2e71d1787ae497693afc3f60d8e16b5289ae
Przemo Firszt <przemo@firszt.eu>
2014-05-05 13:55:51 +0100

    Add description to map.php

--- map.php
+++ map.php
@@ -1,3 +1,7 @@
+<-- This file shows location on map. It has to be called with &key=..... to pull info from correcponding location fie -->
+<html>
+<head> <title>Location</title> </head>
+
 <?php
 	include "settings.php";
 	$key = $_GET['key'];
@@ -7,10 +11,6 @@
 	$lon = $info['lon'];
 ?>
 
-<html>
-<head>
-	<title>Location</title>
-</head>
 <body>
 	<?php echo $info['lat']?>,<?php echo $info['lon']?> </br>
 	<img src= "http://staticmap.openstreetmap.de/staticmap.php?center=<?php echo $info['lat']?>,<?php echo $info['lon']?>&zoom=14&size=800x600&markers=<?php echo $info['lat']?>,<?php echo $info['lon']?>,ol-marker" >
