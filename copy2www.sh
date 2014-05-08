303a3594fc11a74233c6814180085cb49c2a6d63
Przemo Firszt <przemo@firszt.eu>
2014-05-08 21:57:14 +0100

    Add zipping to copy script
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- copy2www.sh
+++ copy2www.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo cp -f manifest.webapp index.html map.php settings.php tracker.php /var/www/geofinder.eu/
+zip -b . geofinder.zip *
+
+sudo cp -f geofinder.zip manifest.webapp index.html map.php settings.php tracker.php /var/www/geofinder.eu/
 sudo cp -f icons/* /var/www/geofinder.eu/icons/
 sudo chown -R www-data:www-data /var/www/geofinder.eu/*
