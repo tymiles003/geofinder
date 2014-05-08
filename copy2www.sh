f4fc126afa193fdc9c89958b54a38269e20d8b95
Przemo Firszt <przemo@firszt.eu>
2014-05-08 20:58:21 +0100

    Move icon to icons/
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- copy2www.sh
+++ copy2www.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo cp -f favicon.ico index.html map.php settings.php tracker.php /var/www/geofinder.eu
+sudo cp -f index.html map.php settings.php tracker.php /var/www/geofinder.eu/
+sudo cp -f icons/favicon.ico /var/www/geofinder.eu/icons/
 sudo chown www-data:www-data /var/www/geofinder.eu/*
