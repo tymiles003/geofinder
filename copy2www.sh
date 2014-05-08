6fb01fdce0ddb64b7e1ba40a7a651173d53c9548
Przemo Firszt <przemo@firszt.eu>
2014-05-08 21:14:03 +0100

    Chown all files in geofinder dir after copying
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- copy2www.sh
+++ copy2www.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo cp -f manifest.webapp index.html map.php settings.php tracker.php /var/www/geofinder.eu/
 sudo cp -f icons/favicon.ico /var/www/geofinder.eu/icons/
-sudo chown www-data:www-data /var/www/geofinder.eu/*
+sudo chown -R www-data:www-data /var/www/geofinder.eu/*
