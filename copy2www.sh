e8cf163b22864e14e39d6ffde91cf935e1e08c2c
Przemo Firszt <przemo@firszt.eu>
2014-05-13 21:27:02 +0100

    move geofinder to main directory
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- copy2www.sh
+++ copy2www.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 zip -b . geofinder.zip *
 
-sudo cp -f geofinder.zip manifest.webapp index.html map.php settings.php tracker.php /var/www/geofinder.eu/
-sudo cp -f icons/* /var/www/geofinder.eu/icons/
-sudo chown -R www-data:www-data /var/www/geofinder.eu/*
+sudo cp -f geofinder.zip manifest.webapp index.html map.php settings.php tracker.php /var/www/
+sudo cp -f icons/* /var/www/icons/
+sudo chown -R www-data:www-data /var/www/*.php
+sudo chown -R www-data:www-data /var/www/*.html
+sudo chown -R www-data:www-data /var/www/manifest.webapp
+sudo chown -R www-data:www-data /var/www/geofinder.zip
