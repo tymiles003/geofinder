e314b1e17909d5e628a4b3d4d339dc37ca62faa7
Przemo Firszt <przemo@firszt.eu>
2014-05-13 22:06:02 +0100

    Move css to separate file
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- copy2www.sh
+++ copy2www.sh
@@ -4,8 +4,10 @@ zip -b . geofinder.zip *
 sudo cp -f geofinder.zip manifest.webapp index.html map.php settings.php tracker.php /var/www/
 sudo cp -f icons/* /var/www/icons/
 sudo cp -f script/* /var/www/script/
-sudo chown -R www-data:www-data /var/www/script/*
+sudo cp -f style/* /var/www/style/
 sudo chown -R www-data:www-data /var/www/icons/*
+sudo chown -R www-data:www-data /var/www/script/*
+sudo chown -R www-data:www-data /var/www/style/*
 sudo chown -R www-data:www-data /var/www/*.php
 sudo chown -R www-data:www-data /var/www/*.html
 sudo chown -R www-data:www-data /var/www/manifest.webapp
