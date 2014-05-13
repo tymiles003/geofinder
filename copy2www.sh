2a0fd6069275793b2c693bf8a6a993da577c345c
Przemo Firszt <przemo@firszt.eu>
2014-05-13 21:31:17 +0100

    Add jquery to cpy file
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- copy2www.sh
+++ copy2www.sh
@@ -3,6 +3,9 @@ zip -b . geofinder.zip *
 
 sudo cp -f geofinder.zip manifest.webapp index.html map.php settings.php tracker.php /var/www/
 sudo cp -f icons/* /var/www/icons/
+sudo cp -f script/* /var/www/script/
+sudo chown -R www-data:www-data /var/www/script/*
+sudo chown -R www-data:www-data /var/www/icons/*
 sudo chown -R www-data:www-data /var/www/*.php
 sudo chown -R www-data:www-data /var/www/*.html
 sudo chown -R www-data:www-data /var/www/manifest.webapp
