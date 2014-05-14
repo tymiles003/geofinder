da6a237d496bcce0830a1fb3e77dddc602449392
Przemo Firszt <przemo@firszt.eu>
2014-05-14 20:58:38 +0100

    Add php -mysql link
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- tracker.php
+++ tracker.php
@@ -18,4 +18,17 @@
 	$fh = fopen($fpath, 'w');
 	fwrite($fh, serialize($info));
 	fclose($fh);
+
+	$con=mysqli_connect("geofinder.eu","tracker","write","location");
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+
+	$sql="INSERT INTO location VALUES (1, NOW(), 10, 20);";
+
+	if (mysqli_query($con,$sql)) {
+		echo "Location written";
+	} else {
+		echo "Error writing location: " . mysqli_error($con);
+	}
 ?>
