1ddad2e8b776aa782287319f2d5d44d52735a70d
Przemo Firszt <przemo@firszt.eu>
2014-05-19 19:46:55 +0100

    Change icons to images
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- copy2www.sh
+++ copy2www.sh
@@ -2,10 +2,10 @@
 zip -b . geofinder.zip *
 
 sudo cp -f geofinder.zip manifest.webapp index.html map.php settings.php tracker.php /var/www/
-sudo cp -f icons/* /var/www/icons/
+sudo cp -f images/* /var/www/images/
 sudo cp -f script/* /var/www/script/
 sudo cp -f style/* /var/www/style/
-sudo chown -R www-data:www-data /var/www/icons/*
+sudo chown -R www-data:www-data /var/www/images/*
 sudo chown -R www-data:www-data /var/www/script/*
 sudo chown -R www-data:www-data /var/www/style/*
 sudo chown -R www-data:www-data /var/www/*.php
