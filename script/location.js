9db6ff5b0a5caeba7962ad7c973f0bf5f2658ecf
Przemo Firszt <przemo@firszt.eu>
2014-05-24 10:38:13 +0100

    Add marker to the map
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- script/location.js
+++ script/location.js
@@ -69,6 +69,14 @@ function show_map(lat, lon) {
 	var zoom           = 14;
 	map.addLayer(osm_layer);
 	map.setCenter(position, zoom );
+
+	var markers = new OpenLayers.Layer.Markers( "Markers" );
+	map.addLayer(markers);
+
+	var size = new OpenLayers.Size(21,25);
+	var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+	var icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+	markers.addMarker(new OpenLayers.Marker(position ,icon));
 }
 
 function toggle_tracking() {
