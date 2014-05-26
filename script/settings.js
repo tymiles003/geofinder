6a143c68184a484980c39f2da281ed41f9b45c8b
Przemo Firszt <przemo@firszt.eu>
2014-05-26 19:44:08 +0100

    Fix SMS button
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- script/settings.js
+++ script/settings.js
@@ -1,4 +1,5 @@
 const server = "http://www.geofinder.eu/";
 const tracker = "tracker.php/";
 const mail_header = "mailto:?subject=GeoFinder%20link&body="
+const sms_header = "sms:?body="
 const tracking_interval = 10000; 
