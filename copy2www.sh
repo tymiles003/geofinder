2d002e79418df0a61f7f1c31025b0891333130dc
Przemo Firszt <przemo@firszt.eu>
2014-05-30 12:04:06 +0100

    Change copy script to copy files to a PREFIXed directory
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- copy2www.sh
+++ copy2www.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+PREFIX="v0.1"
 #zip -b . geofinder.zip *
 
-sudo cp -f manifest.webapp index.html map.php settings.php tracker.php /var/www/
+sudo cp -f manifest.webapp index.html map.php settings.php tracker.php /var/www/$PREFIX/
 
-sudo cp -f images/favicon.ico /var/www/images/
-sudo cp -f images/geofinder-128x128.png /var/www/images/
-sudo cp -f images/geofinder-16x16.png /var/www/images/
-sudo cp -f images/geofinder-256x256.png /var/www/images/
-sudo cp -f images/geofinder-32x32.png /var/www/images/
-sudo cp -f images/geofinder-60x60.png /var/www/images/
-sudo cp -f images/geofinder-90x90.png /var/www/images/
-sudo cp -f images/geofinder.svg /var/www/images/
+sudo cp -f images/favicon.ico /var/www/$PREFIX/images/
+sudo cp -f images/geofinder-128x128.png /var/www/$PREFIX/images/
+sudo cp -f images/geofinder-16x16.png /var/www/$PREFIX/images/
+sudo cp -f images/geofinder-256x256.png /var/www/$PREFIX/images/
+sudo cp -f images/geofinder-32x32.png /var/www/$PREFIX/images/
+sudo cp -f images/geofinder-60x60.png /var/www/$PREFIX/images/
+sudo cp -f images/geofinder-90x90.png /var/www/$PREFIX/images/
+sudo cp -f images/geofinder.svg /var/www/$PREFIX/images/
 
-sudo cp -f script/location.js /var/www/script/
-sudo cp -f script/settings.js /var/www/script/
+sudo cp -f script/location.js /var/www/$PREFIX/script/
+sudo cp -f script/settings.js /var/www/$PREFIX/script/
 
-sudo cp -f style/style.css /var/www/style/
+sudo cp -f style/style.css /var/www/$PREFIX/style/
 
-sudo chown -R www-data:www-data /var/www/images/*
-sudo chown -R www-data:www-data /var/www/script/*
-sudo chown -R www-data:www-data /var/www/style/*
-sudo chown -R www-data:www-data /var/www/*.php
-sudo chown -R www-data:www-data /var/www/*.html
-sudo chown -R www-data:www-data /var/www/manifest.webapp
+sudo chown -R www-data:www-data /var/www/$PREFIX/images/*
+sudo chown -R www-data:www-data /var/www/$PREFIX/script/*
+sudo chown -R www-data:www-data /var/www/$PREFIX/style/*
+sudo chown -R www-data:www-data /var/www/$PREFIX/*.php
+sudo chown -R www-data:www-data /var/www/$PREFIX/*.html
+sudo chown -R www-data:www-data /var/www/$PREFIX/manifest.webapp
 #sudo chown -R www-data:www-data /var/www/geofinder.zip
