e9065bd3a0ea6fa43c522738ea3eae412097d873
Przemo Firszt <przemo@firszt.eu>
2014-06-08 11:01:33 +0100

    Use fa symbols. Transition to Font-Awesome step 2
    Signed-off-by: Przemo Firszt <przemo@firszt.eu>

--- script/settings.js
+++ script/settings.js
@@ -6,10 +6,10 @@ const tracking_interval = 10000;
 const marker_file = "images/blue_marker.png";
 const zoom = 16;
 const locating_str = "Locating...";
-const track_start_str = "Track!";
-const track_stop_str = "Stop!";
-const email_link_str = "Email tracker link";
-const sms_link_str = "SMS tracker link";
+const track_start_str = "<i class=\"fa fa-road\"></i> Track!";
+const track_stop_str = "<i class=\"fa fa-ban\"></i> Stop!";
+const email_link_str = "<i class=\"fa fa-send\"></i> Email tracker link";
+const sms_link_str = "<i class=\"fa fa-share-alt\"></i> SMS tracker link";
 
 var fromProjection = new OpenLayers.Projection ( "EPSG:4326" );
 var toProjection = new OpenLayers.Projection ( "EPSG:900913" );
